7ad57cba951b5d53433fe93fe1645fc069f76fe2
mishhgun01 <AVATARno15>
2022-05-25 00:32:14 +0300

    test push

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -15,6 +15,7 @@ func New(r *mux.Router, s storage.DB) *API {
 	return &API{r: r, db: s}
 }
 
+// Handle API paths.
 func (api *API) Handle() {
 	api.r.HandleFunc("/api/v1/getReviews/{id}", api.reviews).Methods(http.MethodGet, http.MethodOptions)
 	api.r.HandleFunc("/api/v1/newUser", api.newUser).Methods(http.MethodPost, http.MethodOptions)
