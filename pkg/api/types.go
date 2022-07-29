4dbc340c58d2273f98f036d76e96a62f7f59c2a8
Gleb Pimenov <75137969+GarlicRoll@users.noreply.github.com>
2022-07-29 16:50:40 +0300

    Убраны ненужные методы

--- pkg/api/types.go
+++ pkg/api/types.go
@@ -25,45 +25,5 @@ func (api *API) types(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		w.WriteHeader(http.StatusOK)
-
-	case http.MethodPost:
-		var data models.Type
-		err := json.NewDecoder(r.Body).Decode(&data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-		id, err = api.db.NewType(data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(strconv.Itoa(id)))
-	
-	case http.MethodPatch:
-		var data models.Type
-		err := json.NewDecoder(r.Body).Decode(&data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		id, err = api.db.UpdateType(data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(strconv.Itoa(id)))
-		
-	case http.MethodDelete:
-		var data models.Type
-		err := json.NewDecoder(r.Body).Decode(&data)
-		id, err = api.db.DeleteType(data.ID)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(strconv.Itoa(id)))
 	}
 }
