e6a18f684d01ca2886f5b3449ff4430d22b98fd9
Gleb Pimenov <75137969+GarlicRoll@users.noreply.github.com>
2022-07-29 16:50:11 +0300

    Убраны ненужные методы

--- pkg/api/genres.go
+++ pkg/api/genres.go
@@ -26,43 +26,5 @@ func (api *API) genres(w http.ResponseWriter, r *http.Request) {
 		}
 		w.WriteHeader(http.StatusOK)
 
-	case http.MethodPost:
-		var data models.Genre
-		err := json.NewDecoder(r.Body).Decode(&data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-		id, err = api.db.NewGenre(data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(strconv.Itoa(id)))
-	
-	case http.MethodPatch:
-		var data models.Genre
-		err := json.NewDecoder(r.Body).Decode(&data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		id, err = api.db.UpdateGenre(data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(strconv.Itoa(id)))
-		
-	case http.MethodDelete:
-		var data models.Genre
-		err := json.NewDecoder(r.Body).Decode(&data)
-		id, err = api.db.DeleteGenre(data.ID)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(strconv.Itoa(id)))
 	}
+}
