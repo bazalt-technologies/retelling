5bf19519b239cbdf42e09ad30ab8d9056e51f5c4
Gleb Pimenov <75137969+GarlicRoll@users.noreply.github.com>
2022-07-29 16:51:59 +0300

    Убраны ненужные методы

--- pkg/storage/pgsql/types.go
+++ pkg/storage/pgsql/types.go
@@ -5,21 +5,6 @@ import (
 	"retelling/pkg/models"
 )
 
-func (s *Storage) NewType(data models.Type) (int, error) {
-	var id int
-	err := s.pool.QueryRow(context.Background(), `
-	INSERT INTO types (
-		type
-	)
-	VALUES ($1) 
-	RETURNING id
-	`,
-		data.Type).Scan(&id)
-	if err != nil {
-		return -1, err
-	}
-	return id, nil
-}
 
 func (s *Storage) GetTypes(req models.Request) ([]models.Type, error) {
 	var data []models.Review
@@ -46,33 +31,4 @@ func (s *Storage) GetTypes(req models.Request) ([]models.Type, error) {
 		data = append(data, item)
 	}
 	return data, nil
-}
-
-func (s *Storage) UpdateType(data models.Type) (int, error) {
-	var id int
-	err := s.pool.QueryRow(context.Background(), `
-	UPDATE types
-		SET
-		type = $2
-	WHERE id = $1
-	RETURNING id
-	`,
-		data.ID,
-		data.Type).Scan(&id)
-	if err != nil {
-		return -1, err
-	}
-	return id, nil
-}
-
-func (s *Storage) DeleteType(id int) (int, error) {
-	err := s.pool.QueryRow(context.Background(), `
-	DELETE FROM types WHERE id = $1
-  DELETE FROM reviews WHERE type_id = $2
-	`,
-		id, id)
-	if err != nil {
-		return -1, err
-	}
-	return id, nil
 }
