9132323498916a1dad7d8dbd2f183dd72faed671
Gleb Pimenov <75137969+GarlicRoll@users.noreply.github.com>
2022-07-29 16:51:25 +0300

    Убраны ненужные методы

--- pkg/storage/pgsql/genres.go
+++ pkg/storage/pgsql/genres.go
@@ -5,21 +5,6 @@ import (
 	"retelling/pkg/models"
 )
 
-func (s *Storage) NewGenre(data models.Genre) (int, error) {
-	var id int
-	err := s.pool.QueryRow(context.Background(), `
-	INSERT INTO genres (
-		genre
-	)
-	VALUES ($1) 
-	RETURNING id
-	`,
-		data.Genre).Scan(&id)
-	if err != nil {
-		return -1, err
-	}
-	return id, nil
-}
 
 func (s *Storage) GetGenres(req models.Request) ([]models.Genre, error) {
 	var data []models.Review
@@ -48,31 +33,3 @@ func (s *Storage) GetGenres(req models.Request) ([]models.Genre, error) {
 	return data, nil
 }
 
-func (s *Storage) UpdateGenre(data models.Genre) (int, error) {
-	var id int
-	err := s.pool.QueryRow(context.Background(), `
-	UPDATE genres
-		SET
-		genre = $2
-	WHERE id = $1
-	RETURNING id
-	`,
-		data.ID,
-		data.Genre).Scan(&id)
-	if err != nil {
-		return -1, err
-	}
-	return id, nil
-}
-
-func (s *Storage) DeleteGenre(id int) (int, error) {
-	err := s.pool.QueryRow(context.Background(), `
-	DELETE FROM genres WHERE id = $1
-  DELETE FROM reviews WHERE genre_id = $2
-	`,
-		id, id) 
-	if err != nil {
-		return -1, err
-	}
-	return id, nil
-}
