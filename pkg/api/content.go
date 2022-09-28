a23b998c1e6b8c55cf3ec54561a89b00f1593e76
Argentusz <A4rgentum@yandex.ru>
2022-09-28 14:23:15 +0300

    Очепятка исправлена :)

--- pkg/api/content.go
+++ pkg/api/content.go
@@ -10,12 +10,12 @@ import (
 func (api *API) content(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost:
-		var data models.Review
+		var data models.Content
 		err := json.NewDecoder(r.Body).Decode(&data)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-		id, err := api.db.NewReview(data)
+		id, err := api.db.NewContent(data)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
