ccaf87e4f919645e4e37ee96e17fae5042d86b0d
Argentusz <A4rgentum@yandex.ru>
2022-10-07 12:29:01 +0300

    demodata.sql fix

--- pkg/SQL/demodata.sql
+++ pkg/SQL/demodata.sql
@@ -1,15 +1,15 @@
 INSERT INTO users(id, name, login, password, age) VALUES
                                                       (0, 'admin', 'admin','qwerty',21);
 
-INSERT INTO types(0, "Книги");
-INSERT INTO types(1, "Фильмы");
-INSERT INTO types(2, "Сериалы");
-INSERT INTO types(3, "Музыка");
+INSERT INTO types VALUES (0, 'Книги');
+INSERT INTO types VALUES (1, 'Фильмы');
+INSERT INTO types VALUES (2, 'Сериалы');
+INSERT INTO types VALUES (3, 'Музыка');
 
-INSERT INTO genres(0, "Боевик");
-INSERT INTO genres(1, "Вестерн");
-INSERT INTO genres(2, "Детектив");
-INSERT INTO genres(3, "Драма");
-INSERT INTO genres(4, "Комедия");
-INSERT INTO genres(5, "Мелодрама");
-INSERT INTO genres(6, "Трагедия");
+INSERT INTO genres VALUES (0, 'Боевик');
+INSERT INTO genres VALUES (1, 'Вестерн');
+INSERT INTO genres VALUES (2, 'Детектив');
+INSERT INTO genres VALUES (3, 'Драма');
+INSERT INTO genres VALUES (4, 'Комедия');
+INSERT INTO genres VALUES (5, 'Мелодрама');
+INSERT INTO genres VALUES (6, 'Трагедия');
