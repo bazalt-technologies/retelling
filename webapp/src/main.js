c4c7864a370b9c7167b8897c4164c598cf4ae464
mishhgun01 <AVATARno15>
2022-10-22 12:07:28 +0300

    вынесение роутера в отдельный файл

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -6,32 +6,11 @@ Vue.config.productionTip = false
 Vue.prototype.$baseUrl="http://localhost:8081"
 
 import http from "./plugins/http"
-import MainView from "@/components/MainView";
-import LoginComponent from "@/components/LoginComponent";
-import OpenComponent from "@/components/OpenComponent";
-import CompRegistration from "@/components/CompRegistration";
+import routes from "@/routes";
 Vue.use(http, {
   baseUrl: Vue.prototype.$baseUrl
 })
 
-const routes = [
-  {
-    path: "/registration",
-    component: CompRegistration
-  },
-  {
-    path: "/login",
-    component: LoginComponent
-  },
-  {
-    path: "/content",
-    component: MainView
-  },
-  {
-    path: "/",
-    component: OpenComponent
-  }
-]
 const router = new VueRouter({
   history: 'history',
   routes, // short for `routes: routes`
