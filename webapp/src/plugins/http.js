f58338de8bd7ae4a467023285ed8643491cfb964
Gleb Pimenov <gleb.pimenov7@gmail.com>
2022-11-01 21:08:49 +0300

    Фронт обёрнут в контейнер Теперь весь проект можно запустить с помощью запуска докера, docker-compose build, docker-up

--- webapp/src/plugins/http.js
+++ webapp/src/plugins/http.js
@@ -3,7 +3,7 @@ import axios from "axios"
 export default {
     install(Vue, options) {
         Vue.prototype.$http = axios.create({
-            baseUrl: options.baseUrl || "http://localhost:8081",
+            baseUrl: options.baseUrl || "http://server:8081",
             headers: options.headers || null
         })
     }
