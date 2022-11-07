97c9b908fbb08ba277c75e4442870e3015662b94
mishhgun01 <golubev1400@mail.ru>
2022-11-07 15:55:17 +0300

    mobile

--- webapp/src/components/HeaderComponent.vue
+++ webapp/src/components/HeaderComponent.vue
@@ -1,7 +1,33 @@
 <template>
   <div class="header">
     <img v-bind:src="require('../assets/logoRetelling-short.png')" class="header-logo" alt="logo"/>
-    <div class="header-btns">
+    <div v-if="!isMobile" class="header-btns">
+      <ButtonComponent
+          :selected="($route.path==='/content/recommendations') || ($route.path==='/content/search')"
+          :label="'Лента'"
+          :icon="'lenta.svg'"
+          :text-disappear="true"
+          @btnClick="()=>{$router.push('/content/recommendations')}"
+          class="header-btn"
+      ></ButtonComponent>
+      <ButtonComponent
+          :selected="($route.path==='/profile')"
+          :label="'Профиль'"
+          :icon="'profile.svg'"
+          :text-disappear="true"
+          @btnClick="()=>{$router.push('/profile')}"
+          class="header-btn"
+      ></ButtonComponent>
+      <ButtonComponent
+          :selected="($route.path==='/settings')"
+          :label="'Настройки'"
+          :icon="'settings.svg'"
+          :text-disappear="true"
+          @btnClick="$router.push('/settings')"
+          class="header-btn"
+      ></ButtonComponent>
+    </div>
+    <div v-if="isMobile" class="mobile">
       <ButtonComponent
           :selected="($route.path==='/content/recommendations') || ($route.path==='/content/search')"
           :label="'Лента'"
@@ -36,6 +62,25 @@ export default {
   name: "HeaderComponent",
   components: {
     ButtonComponent,
+  },
+  data(){
+    return {
+      width: 0
+    }
+  },
+  computed: {
+    isMobile() {
+      return this.width<500
+    }
+  },
+  created() {
+    this.width = window.innerWidth;
+    window.addEventListener('resize', this.updateWidth);
+  },
+  methods: {
+    updateWidth() {
+        this.width = window.innerWidth;
+    }
   }
 }
 </script>
@@ -48,6 +93,7 @@ export default {
   background-color: #94d1be;
   margin: 0;
   padding: 5px;
+  width: 100%;
 }
 .header-btns {
   display: flex;
@@ -57,9 +103,21 @@ export default {
 }
 .header-btn {
   margin-right: 1vw;
+  border: 0px;
 }
 .header-logo {
   height: 4vw;
-  min-height: 40px;
+}
+.mobile{
+  display: flex;
+  position: fixed;
+  bottom: 0;
+  justify-content: space-between;
+  z-index: 100;
+  padding: 0;
+  margin: 0;
+  align-content: center;
+  background-color: #94d1be;
+  width: 100%;
 }
 </style>
