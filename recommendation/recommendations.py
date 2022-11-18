106f9a2bf1125c4b860197956c873298d451e152
icekomar <miha.komar@mail.ru>
2022-11-18 13:32:50 +0300

    комментарии

--- recommendation/recommendations.py
+++ recommendation/recommendations.py
@@ -30,6 +30,7 @@ class Recommendation(Resource):
         # Пользователя с UserID нету или у него нет likes
         if objects is None:
             abort(404)
+        # запись в список ID объектов ,которые лайкнул пользователь
         user_likes = [objects[j]["ID"] for j in range(len(objects))]
         # Пользователи, которые лайкнули контент (случайный, из тех который лайкнули мы), такой же как мы
         users = []
@@ -47,10 +48,14 @@ class Recommendation(Resource):
             abort(500)
 
         if len(users) > 1:
+            #коэффициент Танимото
             coef = 0
             k = 0
+            #Нужное количество схожих пользователей
             n = len(users) // 2
+            # Коэфицент для сравнивания
             SimilarCoef = 0.9
+            # словари для записи лайков других пользователей и их коэффицент Танимото
             DictionaryLikes = {}
             DictionaryCoefs = {}
             for i in range(len(users)):
@@ -58,11 +63,14 @@ class Recommendation(Resource):
                 objects2 = json.loads(r.content.decode())
                 if objects2 is None:
                     continue
+                #лайки другого пользователя
                 user_likes2 = [objects2[j]["ID"] for j in range(len(objects2))]
-                b = (set(user_likes2) - set(user_likes))
-                coef = len(b) / (len(user_likes) + len(user_likes2) - len(b))
-                DictionaryLikes[users[i]] = b
+                #Общие лайки
+                SameLikes = (set(user_likes2) - set(user_likes))
+                coef = len(SameLikes) / (len(user_likes) + len(user_likes2) - len(SameLikes))
+                DictionaryLikes[users[i]] = SameLikes
                 DictionaryCoefs[users[i]] = coef
+            #сравнение схожести пользователей
             while SimilarCoef != 0.3 and k < n:
                 for i in range(len(users)):
                     if DictionaryCoefs.get(users[i]) >= SimilarCoef:
@@ -71,6 +79,7 @@ class Recommendation(Resource):
                         del DictionaryCoefs[users[i]]
                         del DictionaryLikes[users[i]]
                 SimilarCoef -= 0.2
+            #Если не выбралось нужное количество пользователей
             if k < n:
                 for j in range(n - k):
                     key = random.choice(list(DictionaryLikes.keys()))
